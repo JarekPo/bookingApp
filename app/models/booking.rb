c06af9ea9db7bdbaf5c0560e7117b7615a5ac028
Jaroslaw Polowy <jarekpolowy@gmail.com>
2022-05-06 20:48:59 +0100

    date and number of pallets validation fields added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ApplicationRecord
     validates_presence_of :Supplier_name, :Number_of_pallets, :Date
     belongs_to :user
-end
+
+    validates :Date,
+    Date: { after: Proc.new { Date.today }, message: 'Must be after today' },
+    on: :create
+
+
+    validates :Number_of_pallets,
+    numericality: { greater_than:0,  less_than_or_equal_to: 26, only_integer: true }
+end 
